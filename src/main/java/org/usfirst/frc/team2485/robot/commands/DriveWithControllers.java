badf2c16c7ec854ac7f81ef183fcf632232165da
Hanna Dorsey <skumar2019@francisparker.org>
2019-04-08 21:13:31 -0700

    drive w/ controllers omni wheels

--- src/main/java/org/usfirst/frc/team2485/robot/commands/DriveWithControllers.java
+++ src/main/java/org/usfirst/frc/team2485/robot/commands/DriveWithControllers.java
@@ -19,23 +19,23 @@ public class DriveWithControllers extends edu.wpi.first.wpilibj.command.Command 
   protected void execute() {
     double throttle = OI.getDriveThrottle();
     double steering = OI.getDriveSteering();
-    System.out.println("steering: " + steering);
     boolean quickTurn = OI.getQuickTurn();
     boolean slowTurn = OI.jacket.getRawButton(OI.XBOX_B_PORT);
+
     
-    if (!(quickTurn || slowTurn)) {
+    
+    if (!quickTurn && !slowTurn) {
       //steering = org.usfirst.frc.team2485.util.ThresholdHandler.deadbandAndScale(OI.jacket.getRawAxis(0), 0.2, 0.0, 1.0);
-      steering *= Math.abs(OI.getDriveSteering());
+      
+      steering /= 35;
     }
-    // if(quickTurn){
-    //   steering *= 0.5;
-    // }
 
     if (slowTurn) {
-      steering *= 0.;
+      steering *= 0.4;
+      //steering *= 0.5;
     }
 
-    if(RobotMap.elevatorEncoderWrapperDistance.pidGet() >= ElevatorLevel.ROCKET_LEVEL_TWO.getPosition()) {
+    if(RobotMap.elevatorEncoderWrapperDistance.pidGet() >= ElevatorLevel.ROCKET_LEVEL_TWO.getPosition() && RobotMap.driveTrain.teleopSetpointRightRamp.isQuadratic()) {
       double upRamp = ConstantsIO.teleopUpRampElevatorUp + (ConstantsIO.teleopUpRamp - ConstantsIO.teleopUpRampElevatorUp) * (ElevatorLevel.ROCKET_LEVEL_THREE.getPosition() - RobotMap.elevatorEncoderWrapperDistance.pidGet())/ElevatorLevel.ROCKET_LEVEL_THREE.getPosition();
       double downRamp = ConstantsIO.teleopDownRampElevatorUp + (ConstantsIO.teleopDownRamp - ConstantsIO.teleopDownRampElevatorUp) * (ElevatorLevel.ROCKET_LEVEL_THREE.getPosition() - RobotMap.elevatorEncoderWrapperDistance.pidGet())/ElevatorLevel.ROCKET_LEVEL_THREE.getPosition();
   
